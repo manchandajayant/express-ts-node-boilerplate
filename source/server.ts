712ef79de3fe69c4ef64afeb7a8fc0edaec873a2
manchandajayant <maanjayant@gmail.com>
2021-08-04 23:43:15 +0530

    request log routes created,origin rules initiated, error logs initiated, server initiated

---
+++ source/server.ts
@@ -0,0 +1,60 @@
+import http from 'http';
+import express from 'express';
+import logging from './config/logging';
+import config from './config/config';
+
+const NAMESPACE = 'server';
+const router = express();
+
+/**
+ * Logging the request
+ */
+
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`);
+    });
+});
+
+/**
+ * Parse the Request
+ */
+
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+/**
+ * Rules of the API
+ */
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
+        return res.status(200).json({});
+    }
+});
+
+/**
+ * Routes
+ */
+
+/** Error Handling */
+
+router.use((req, res, next) => {
+    const error = new Error('not found');
+
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+/**
+ * Server
+ */
+
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`));
